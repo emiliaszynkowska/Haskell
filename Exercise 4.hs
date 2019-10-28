0e2a227ceae353889b2ccfd78650032c70ec253f
Emilia <emiliaszynkowska@outlook.com>
2019-10-28 13:32:41 +0000

    Update Exercise 4.hs

--- Exercise 4.hs
+++ Exercise 4.hs
@@ -2,7 +2,7 @@ import qualified Data.List as S
 --module Exercises (neighbours) where
 
 --exercise 4
-type Point a = (a,a)
+type Point a = (Int,Int)
 type Metric a = Point a -> Point a -> Double
 --takes two points and returns the distance between them
 metric (a,b) (c,d) = sqrt $ ((c-a)^2 + (d-b)^2) 
@@ -11,8 +11,10 @@ metric (a,b) (c,d) = sqrt $ ((c-a)^2 + (d-b)^2)
 -- neighbours k d p xs = 
 
 --gets all neighbours, ordered by distance
-allNeighbours p xs (r:rs) = fst r : allNeighbours p xs rs
+allNeighbours p [] = []
+allNeighbours p xs = r : allNeighbours p (drop 1 rs)
     where rs = orderByDistance p xs
+          r = snd(rs!!0)
 
 --sorts mapDistance
 orderByDistance p xs = S.sort (mapDistance p xs)
